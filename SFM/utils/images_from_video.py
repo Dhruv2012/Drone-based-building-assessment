54ff759ed70fbbdb32b57ce256f427f57e3805b3
Kush0301 <kushagra2000@gmail.com>
2022-03-22 05:31:01 +0530

    Util function to get frames from a video

---
+++ SFM/utils/images_from_video.py
@@ -0,0 +1,44 @@
+# Importing all necessary libraries
+import cv2
+import os
+
+# Read the video from specified path
+cam = cv2.VideoCapture("/home/kushagra/IIIT-H/SFM/DJI_0232/DJI_0232.MP4")
+
+# Output Folder
+outputloc = "/home/kushagra/IIIT-H/SFM/DJI_0232/"
+try:
+
+    # creating a folder named data
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+# if not created then raise error
+except OSError:
+    print('Error: Creating directory of data')
+
+# frame
+currentframe = 0
+while True:
+
+    # reading from frame
+    ret, frame = cam.read()
+
+    if ret:
+
+        # if video is still left continue creating images
+        name = outputloc+"/%#05d.jpg" % (currentframe+1)
+        print('Creating...' + name)
+
+        # writing the extracted images
+        cv2.imwrite(name, frame)
+
+        # increasing counter so that it will
+        # show how many frames are created
+        currentframe += 1
+    else:
+        break
+
+# Release all space and windows once done
+cam.release()
+cv2.destroyAllWindows()
