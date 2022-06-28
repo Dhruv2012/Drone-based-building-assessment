1933fe929e3e9ee9e7ae8066aa6298f50f47b9eb
Kush0301 <kushagra2000@gmail.com>
2022-06-28 18:26:22 +0530

    Function to find transformation data

--- ImageStitching/utils.py
+++ ImageStitching/utils.py
@@ -1,5 +1,7 @@
+from cv2 import transform
 import numpy as np
 import pandas as pd
+import geopy.distance
 
 def skew_symmetric(v):
 
@@ -17,6 +19,7 @@ class Quaternion():
             self.z=z
 
         else:
+            euler = np.deg2rad(euler)
             roll=euler[0]
             pitch=euler[1]
             yaw=euler[2]
@@ -84,3 +87,67 @@ def ORBReader(file_path):
     column_names = ['time', 'x', 'y', 'z', 'qx', 'qy', 'qz', 'qw']
     data = pd.read_csv(file_path, sep=' ', names=column_names)
     return data
+
+def GetScale(prev_coords, current_cords):
+
+    #Input:
+    #prev_coords = (prev_lat, prev_lon)
+    #current_cords = (current_lat, current_lon)
+
+    #Output:
+    #scale = distance traveled in meters
+
+    scale = abs(geopy.distance.distance(prev_coords, current_cords).m)
+    return scale
+
+def GetTransformationMatrix(translation, quaternion, scale):
+    
+    #Input:
+    #translation = (x,y,z)
+    #quaternion = (w,x,y,z)
+    #Output:
+    #transformation_matrix = (4x4)
+
+    quaternion = Quaternion(*quaternion)
+    quat_mat = quaternion.to_mat()
+
+    transformation_matrix = np.eye(4)
+    transformation_matrix[:3,:3] = quat_mat
+    transformation_matrix[:3,3] = translation
+    transformation_matrix[0:3,3] = transformation_matrix[0:3,3]*scale
+
+    return transformation_matrix
+
+
+
+def GetHomographyData(orb_data,log_data):
+    
+    transformation_matrices={}
+
+    #Adding reference frame data
+    ref_translation = [orb_data['x'][0], orb_data['y'][0], orb_data['z'][0]]
+    ref_quat = [orb_data['qw'][0], orb_data['qx'][0], orb_data['qy'][0], orb_data['qz'][0]]
+    ref_scale = 1
+    ref_transformation_matrix = GetTransformationMatrix(ref_translation, ref_quat, ref_scale)
+
+    transformation_matrices[log_data['time'][0]] = [ref_transformation_matrix, log_data['image_name'][0], log_data['height'][0]]
+
+    for index, i in orb_data.iterrows():
+
+
+        ind = int(np.where(log_data['time']/1000==i['time'])[0])
+        
+        if int(ind)!=0:
+            
+            current_coords = (log_data['latitude'][ind], log_data['longitude'][ind])
+            prev_coords = (log_data['latitude'][ind-1], log_data['longitude'][ind-1])
+            scale = GetScale(prev_coords, current_coords)
+
+            translation = [orb_data['x'][index], orb_data['y'][index], orb_data['z'][index]]
+            quat = [orb_data['qw'][index], orb_data['qx'][index], orb_data['qy'][index], orb_data['qz'][index]]
+            transformation_matrix = GetTransformationMatrix(translation, quat, scale)
+            
+            print(transformation_matrix)
+            transformation_matrices[i['time']] = [transformation_matrix, log_data['image_name'][ind], log_data['height'][ind]]
+    
+    return transformation_matrices
